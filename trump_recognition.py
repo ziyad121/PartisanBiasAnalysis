fba674bee7177538f7335445fed7c29a0cc777f8
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-21 18:08:55 +0200

    Update trump_recognition.py

--- trump_recognition.py
+++ trump_recognition.py
@@ -1,23 +1,33 @@
-#install python 3.6
-#using cmd :
-#pip install CMake
-#install dlib package using the link for windows users
-#https://github.com/charlielito/install-dlib-python-windows
-#pip install --no-dependencies face_recognition face_recognition_models
 
 import newsapi_scrapping as ns
-from urllib.request import urlopen
-from PIL import Image
-import face_recognition
+import os 
+import urllib.request
+#import face_recognition
 
-known_image = face_recognition.load_image_file("biden.jpg")
-unknown_image = face_recognition.load_image_file("unknown.jpg")
+#known_image = face_recognition.load_image_file("biden.jpg")
+#unknown_image = face_recognition.load_image_file("unknown.jpg")
 
-biden_encoding = face_recognition.face_encodings(known_image)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+#biden_encoding = face_recognition.face_encodings(known_image)[0]
+#unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+#results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+if not os.path.exists('images'): 
+    os.mkdir('images')
 
-#for i in range(len(ns.df_left)):
-img = Image.open(urlopen(ns.df_left['urlToImage'][0]))
+def downloader(image_url,file_name):
+    full_file_name = str(file_name) + '.jpg'
+    full = os.path.join(os.getcwd()+'/images/', full_file_name )
+    urllib.request.urlretrieve(image_url,full)
+
+for i in range(len(ns.df_left)):
+    if print(str(ns.df_left['urlToImage'][i])) == 'None': 
+        i= i + 1 
+    elif ns.df_left['urlToImage'][i]== None :
+        i= i + 1
+    else:    
+        img = downloader(ns.df_left['urlToImage'][i],i)
+
+#for i in range(len(ns.df_right)):
+# img = urllib.request.urlretrieve(ns.df_right['urlToImage'][i], str(len(ns.df_right)+i)+'.jpg')
+
 
