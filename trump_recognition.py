5e0806f5bb3f0d81dda4038b7817e602c168d42c
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-25 11:19:53 +0200

    Update trump_recognition.py

--- trump_recognition.py
+++ trump_recognition.py
@@ -24,9 +24,10 @@ known_image = face_recognition.load_image_file("trump.jpg")
 unknown_image = face_recognition.load_image_file("1.jpg")
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)
 
 if len(unknown_encodings) > 0:
+    unknown_encoding = unknown_encodings[0]
     results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
 else:
    print("No faces found in the image!")
