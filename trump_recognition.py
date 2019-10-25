1e90a93bffb16b24c7c198f7978714bd290943f0
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-25 10:42:56 +0200

    Update trump_recognition.py

--- trump_recognition.py
+++ trump_recognition.py
@@ -20,7 +20,7 @@ def downloader(image_url,file_name):
 img = downloader(ns.df_left['urlToImage'][1],1)
 
 known_image = face_recognition.load_image_file("trump.jpg")
-unknown_image = face_recognition.load_image_file("1.jpg")
+unknown_image = face_recognition.load_image_file("/images/1.jpg")
 
 biden_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
