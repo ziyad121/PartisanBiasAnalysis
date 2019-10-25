74ad14beaffab3861cbedff900ffcd9819dc5d79
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-25 10:35:40 +0200

    Update trump_recognition.py

--- trump_recognition.py
+++ trump_recognition.py
@@ -2,15 +2,7 @@
 import newsapi_scrapping as ns
 import os 
 import urllib.request
-#import face_recognition
-
-#known_image = face_recognition.load_image_file("biden.jpg")
-#unknown_image = face_recognition.load_image_file("unknown.jpg")
-
-#biden_encoding = face_recognition.face_encodings(known_image)[0]
-#unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-
-#results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+import face_recognition
 if not os.path.exists('images'): 
     os.mkdir('images')
 
@@ -19,15 +11,22 @@ def downloader(image_url,file_name):
     full = os.path.join(os.getcwd()+'/images/', full_file_name )
     urllib.request.urlretrieve(image_url,full)
 
-for i in range(len(ns.df_left)):
-    if print(str(ns.df_left['urlToImage'][i])) == 'None': 
-        i= i + 1 
-    elif ns.df_left['urlToImage'][i]== None :
-        i= i + 1
-    else:    
-        img = downloader(ns.df_left['urlToImage'][i],i)
+#for i in range(len(ns.df_left)):
+#    if print(str(ns.df_left['urlToImage'][i])) == 'None': 
+#        i= i + 1 
+#    elif ns.df_left['urlToImage'][i]== None :
+#        i= i + 1
+#    else:    
+img = downloader(ns.df_left['urlToImage'][1],1)
 
-#for i in range(len(ns.df_right)):
-# img = urllib.request.urlretrieve(ns.df_right['urlToImage'][i], str(len(ns.df_right)+i)+'.jpg')
+known_image = face_recognition.load_image_file("trump.jpg")
+unknown_image = face_recognition.load_image_file("1.jpg")
 
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+
+
+print(results)
 
