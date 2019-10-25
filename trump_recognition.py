e094ccc91781c17297a8fa67d491bcfd3ebdf90e
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-25 11:34:39 +0200

    Update trump_recognition.py

--- trump_recognition.py
+++ trump_recognition.py
@@ -3,39 +3,36 @@ import newsapi_scrapping as ns
 import os 
 import urllib.request
 import face_recognition
+
+known_image = face_recognition.load_image_file("trump.jpg")
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+
 if not os.path.exists('images'): 
     os.mkdir('images')
+    
+os.chdir('images')
 
 def downloader(image_url,file_name):
     full_file_name = str(file_name) + '.jpg'
-    #full = os.path.join(os.getcwd()+'/images/', full_file_name)
-    full = os.path.join(os.getcwd(), full_file_name )
+    full = os.path.join(os.getcwd(), full_file_name)
     urllib.request.urlretrieve(image_url,full)
 
-#for i in range(len(ns.df_left)):
-#    if print(str(ns.df_left['urlToImage'][i])) == 'None': 
-#        i= i + 1 
-#    elif ns.df_left['urlToImage'][i]== None :
-#        i= i + 1
-#    else:    
-img = downloader(ns.df_left['urlToImage'][16],16)
+for i in range(len(ns.df_left)):
+    if print(str(ns.df_left['urlToImage'][i])) == 'None': 
+        pass 
+    elif ns.df_left['urlToImage'][i]== None :
+        pass
+    else:    
+        img = downloader(ns.df_left['urlToImage'][i],i)
+        unknown_image = face_recognition.load_image_file(i+'.jpg')
+        unknown_encoding = face_recognition.face_encodings(unknown_image)
+        if len(unknown_encoding) > 0:
+            unknown_encoding = unknown_encodings[0]
+            results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+            print(results)
+        else:
+            print("No faces found in the image!")
+ 
 
-known_image = face_recognition.load_image_file("trump.jpg")
-unknown_image = face_recognition.load_image_file("16.jpg")
-
-biden_encoding = face_recognition.face_encodings(known_image)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image)
-
-if len(unknown_encoding) > 0:
-    unknown_encoding = unknown_encodings[0]
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-else:
-   print("No faces found in the image!")
-   quit()
-
-
-
-
-print(results)
 
 
